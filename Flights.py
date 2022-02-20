75840d4c65813bc445a6fe6a0d1ffa64a2009f5e
mossuser <redhen@mailbox.org>
2022-02-20 12:22:03 +0000

    third commit

--- Flights.py
+++ Flights.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 df=pd.read_csv(r'C:\UCD_2022\flights.csv')
 #print(flights)
 print(df.head())
+df.dtypes
+df.info
+
 
 plt.plot(df['2018'])
 plt.plot(df['2019'])
@@ -13,4 +16,6 @@ plt.plot(df['2035'])
 plt.plot(df['2040'])
 
 plt.xticks(range(0,24,1))
+plt.grid(True)
+plt.legend(['2018', '2019','2025','2030','2035', '2040'])
 plt.show()
